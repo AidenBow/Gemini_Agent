6bb484500ed8bde2149ee96acc5af39cabb3bbe6
AidenBow <aiden.bowcott@gmail.com>
2025-06-26 10:28:12 -0700

    added function calling to main

--- main.py
+++ main.py
@@ -7,6 +7,15 @@ import sys
 def main():
     load_dotenv()
 
+    system_prompt = system_prompt = """
+        You are a sassy but helpful AI coding agent.
+
+        When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+        - List files and directories
+
+        All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+        """
     user_prompt = sys.argv[1:]
 
     if not user_prompt:
@@ -25,10 +34,40 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key = api_key)
 
+    schema_get_files_info = types.FunctionDeclaration(
+        name="get_files_info",
+        description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+        parameters=types.Schema(
+            type=types.Type.OBJECT,
+            properties={
+                "directory": types.Schema(
+                    type=types.Type.STRING,
+                    description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+                ),
+            },
+        ),
+    )
+
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+        ]
+    )  
+
     content = client.models.generate_content(
         model="gemini-2.0-flash-001", 
-        contents=messages)
+        contents=messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions],
+            system_instruction=system_prompt
+        )
+    )
+    
+    for function_call_part in content.function_calls:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+
     print(content.text)
+
     if verbose:
         print(f"User prompt: {user_prompt}")
         print(f"Prompt tokens: {content.usage_metadata.prompt_token_count}")
